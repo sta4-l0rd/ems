1e7b82a89b16bcb5100c39838b55cc4240540d91
Astitwa Kumar <astitwa.kumar@u2opiamobile.com>
2025-06-15 19:59:20 +0530

    Removed unused imports

--- src/main/java/com/sta4l0rd/ems/service/EmployeeService.java
+++ src/main/java/com/sta4l0rd/ems/service/EmployeeService.java
@@ -2,7 +2,6 @@ package com.sta4l0rd.ems.service;
 
 import com.sta4l0rd.ems.empDTO.EmployeeAllDetailsDTO;
 import com.sta4l0rd.ems.empDTO.EmployeesListDTO;
-import com.sta4l0rd.ems.entity.Employee;
 
 public interface EmployeeService {
     public EmployeesListDTO getAllEmployees();
