8d9494099b36765adeabc587e74f73ec3c3fb5ec
Astitwa Kumar <astitwa.kumar@u2opiamobile.com>
2025-06-15 19:53:28 +0530

    Separate Controller for REST APIs

---
+++ src/main/java/com/sta4l0rd/ems/controller/EmployeeRestController.java
@@ -0,0 +1,10 @@
+package com.sta4l0rd.ems.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+}
