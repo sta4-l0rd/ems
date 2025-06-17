afe72370ae54fc01045be73fdc2e508ddc7f4931
Astitwa Kumar <astitwa.kumar@u2opiamobile.com>
2025-06-17 10:22:50 +0530

    Changed api url too root for embedded frontend

--- src/main/java/com/sta4l0rd/ems/controller/EmployeeController.java
+++ src/main/java/com/sta4l0rd/ems/controller/EmployeeController.java
@@ -1,36 +1,42 @@
 package com.sta4l0rd.ems.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sta4l0rd.ems.empDTO.EmployeeAllDetailsDTO;
 import com.sta4l0rd.ems.service.serviceImpl.EmployeeServiceImpl;
 
 @Controller
-@RequestMapping("/ui-api")
+@RequestMapping("")
 public class EmployeeController {
 
     @Autowired
     EmployeeServiceImpl eImpl;
 
     @GetMapping("/employee-list")
-    public ResponseEntity<?> getAll() {
-        return new ResponseEntity<>(eImpl.getAllEmployees(), HttpStatus.OK);
+    public String getAll(Model model) {
+        model.addAttribute("employees", eImpl.getAllEmployees());
+        return "employee-list";
+    }
+
+    @GetMapping("/new-employee-form")
+    public String showEmployeeForm(Model model) {
+        model.addAttribute("employee", new EmployeeAllDetailsDTO());
+        return "add-employee";
     }
 
     @PostMapping("/new-employee")
-    public ResponseEntity<?> createEmployee(@RequestBody EmployeeAllDetailsDTO newEmployee) {
-        if(eImpl.createEmployee(newEmployee)!= null){
-            return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
-        }
-        else{
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    public String createEmployee(@ModelAttribute EmployeeAllDetailsDTO newEmployee, Model model) {
+        if (eImpl.createEmployee(newEmployee) != null) {
+            return "redirect:/employee-list";
+        } else {
+            model.addAttribute("error", "Failed to create employee. Please check your input.");
+            return "add-employee";
         }
     }
 }
